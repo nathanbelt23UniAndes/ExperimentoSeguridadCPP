7d16a9ff364e42d739574f4f4e9e9e8f9c2f7c77
ELISA ANGULO <e.angulov@uniandes.edu.co>
2023-03-13 22:44:28 -0500

    Se desarrolla la vista Usuario para que pueda acceder al api Gate way

--- Usuario/vistas/vista_usuario.py
+++ Usuario/vistas/vista_usuario.py
@@ -1,17 +1,19 @@
 from flask_jwt_extended import  create_access_token
-from flask import request
+from flask import request, jsonify
+import json
 import hashlib
 from flask_restful import Resource
 import requests
 
 
-API_BASE_URL = 'http://localhost:5000/solicitud'
+API_BASE_URL = 'http://localhost:5000'
 
 class vistaUsuario(Resource):
     def post (self):
         response = requests.post(f'{API_BASE_URL}/login', json={'username': request.json["usuario"], 'password': request.json["contrasena"]})
         if response.status_code == 200:
-            token = response.json()['token']
-            print(f'Token de acceso: {token}')
+            data = response.json()
+            return jsonify(data)
+
         else:
             return "Error de autenticacion",404
